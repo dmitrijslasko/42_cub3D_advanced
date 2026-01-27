cf3531a79d1a57ff5623ccf52b00ce6f45cf6d47
Dmitrijs Lasko (XCode) <dmitrijs.lasko@gmail.com>
2026-01-27 23:19:51 +0100

    upd

--- src/main.c
+++ src/main.c
@@ -6,38 +6,38 @@
 /*   By: dmlasko <dmlasko@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 00:12:07 by fvargas           #+#    #+#             */
-/*   Updated: 2025/07/23 18:40:59 by dmlasko          ###   ########.fr       */
+/*   Updated: 2026/01/27 23:19:30 by dmlasko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "sound.h"
 
-#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
+// #define MINIAUDIO_IMPLEMENTATION
+// #include "miniaudio.h"
 
-ma_engine engine;
-ma_sound sound;
+// ma_engine engine;
+// ma_sound sound;
 
-int init_audio(void)
-{
-    if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
-        return -1;
-    }
-    if (ma_sound_init_from_file(&engine, "./wassets/sounds/music2.mp3", 0, NULL, NULL, &sound) != MA_SUCCESS) {
-        ma_engine_uninit(&engine);
-        return -1;
-    }
-    // Enable infinite looping
-    ma_sound_set_looping(&sound, MA_TRUE);
-    ma_sound_start(&sound);
-    return 0;
-}
+// int init_audio(void)
+// {
+//     if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
+//         return -1;
+//     }
+//     if (ma_sound_init_from_file(&engine, "./wassets/sounds/music2.mp3", 0, NULL, NULL, &sound) != MA_SUCCESS) {
+//         ma_engine_uninit(&engine);
+//         return -1;
+//     }
+//     // Enable infinite looping
+//     ma_sound_set_looping(&sound, MA_TRUE);
+//     ma_sound_start(&sound);
+//     return 0;
+// }
 
-void free_audio(void)
-{
-    ma_engine_uninit(&engine);
-}
+// void free_audio(void)
+// {
+//     ma_engine_uninit(&engine);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -79,7 +79,7 @@ int	main(int argc, char **argv)
 	get_init_player_position(dt.map, &dt.player);
 	printf("Game status set!\n");
 
-	draw_minimap_base_img(&dt);
+	// draw_minimap_base_img(&dt);
 
 	print_separator(3, DEF_SEPARATOR_CHAR);
 
@@ -89,7 +89,7 @@ int	main(int argc, char **argv)
 	printf("Consumables to collect in this level: %d\n", get_curr_level(&dt)->starting_level_consumable_count);
 	print_separator(1, DEF_SEPARATOR_CHAR);
 
-	init_audio();
+	// init_audio();
 	
 	mlx_loop_hook(dt.mlx_ptr, &render_frame, &dt);
 	mlx_loop(dt.mlx_ptr);
