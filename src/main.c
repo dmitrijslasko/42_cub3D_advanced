2222f6e87e2181501c8c7558391d8c351288ffd7
Dmitrijs Lasko (XCode) <dmitrijs.lasko@gmail.com>
2026-01-27 23:20:45 +0100

    upd

--- src/main.c
+++ src/main.c
@@ -6,38 +6,38 @@
 /*   By: dmlasko <dmlasko@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 00:12:07 by fvargas           #+#    #+#             */
-/*   Updated: 2026/01/27 23:19:30 by dmlasko          ###   ########.fr       */
+/*   Updated: 2026/01/27 23:20:35 by dmlasko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "sound.h"
 
-// #define MINIAUDIO_IMPLEMENTATION
-// #include "miniaudio.h"
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
 
-// ma_engine engine;
-// ma_sound sound;
+ma_engine engine;
+ma_sound sound;
 
-// int init_audio(void)
-// {
-//     if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
-//         return -1;
-//     }
-//     if (ma_sound_init_from_file(&engine, "./wassets/sounds/music2.mp3", 0, NULL, NULL, &sound) != MA_SUCCESS) {
-//         ma_engine_uninit(&engine);
-//         return -1;
-//     }
-//     // Enable infinite looping
-//     ma_sound_set_looping(&sound, MA_TRUE);
-//     ma_sound_start(&sound);
-//     return 0;
-// }
+int init_audio(void)
+{
+    if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
+        return -1;
+    }
+    if (ma_sound_init_from_file(&engine, "./wassets/sounds/music2.mp3", 0, NULL, NULL, &sound) != MA_SUCCESS) {
+        ma_engine_uninit(&engine);
+        return -1;
+    }
+    // Enable infinite looping
+    ma_sound_set_looping(&sound, MA_TRUE);
+    ma_sound_start(&sound);
+    return 0;
+}
 
-// void free_audio(void)
-// {
-//     ma_engine_uninit(&engine);
-// }
+void free_audio(void)
+{
+    ma_engine_uninit(&engine);
+}
 
 int	main(int argc, char **argv)
 {
